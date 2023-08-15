e95a9bc76a9c4a94c015f56126c24d7864ee8fbe
Silas Coley <silascoley3@gmail.com>
2023-08-15 16:15:53 -0500

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+const PORT = 3000;
+  
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
